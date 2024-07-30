326a381e1fdd8ae5eb66cc0327e7a5de1446447a
Twicipi <m.wifi.8d@gmail.com>
2024-07-30 16:53:39 +0700

    Geshidan - Just Stop Procrastinating

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto bg-gradient-to-r from-slate-900 from-5% via-neutral-800 via-60%  to-slate-700 to-80%">
       <h1 className="font-bold text-5xl text-center text-white">Geshidan</h1>
-      <p className="font-bold text-center mb-5 text-white">Stop Procrastinating!</p>
+      <p className="font-bold text-center mb-5 text-white">
+        Stop Procrastinating!       
+      </p>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo} />
         <TodoList
